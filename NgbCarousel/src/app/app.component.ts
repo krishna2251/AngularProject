4b16c0c1f4f090902ddf42a23105923bb83e376b
krishna2251 <y.narasimha845@gmail.com>
2019-11-12 16:19:56 +0530

    angular projects

---
+++ NgbCarousel/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+}
